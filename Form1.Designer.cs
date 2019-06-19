990f50e09ffe77c289b0b012ea7ba40211192534
c.esneault <c.esneault@oodrive.com>
2019-06-19 10:33:50 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SkiaWinform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.skControl1 = new SkiaSharp.Views.Desktop.SKControl();
+            this._skControl = new SkiaSharp.Views.Desktop.SKControl();
+            this.SuspendLayout();
+            // 
+            // skControl1
+            // 
+            this.skControl1.Location = new System.Drawing.Point(0, 0);
+            this.skControl1.Name = "skControl1";
+            this.skControl1.Size = new System.Drawing.Size(75, 23);
+            this.skControl1.TabIndex = 0;
+            this.skControl1.Text = "skControl1";
+            // 
+            // _skControl
+            // 
+            this._skControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._skControl.Location = new System.Drawing.Point(0, 0);
+            this._skControl.Name = "_skControl";
+            this._skControl.Size = new System.Drawing.Size(802, 452);
+            this._skControl.TabIndex = 1;
+            this._skControl.Text = "skControl2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._skControl);
+            this.Controls.Add(this.skControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SkiaSharp.Views.Desktop.SKControl skControl1;
+        private SkiaSharp.Views.Desktop.SKControl _skControl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using SkiaSharp;
+using SkiaSharp.Views.Desktop;
+
+namespace SkiaWinform
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            _skControl.PaintSurface += OnPaintSurface;
+        }
+
+        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var icon = SystemIcons.Information;
+            var skBitmap = icon.ToBitmap().ToSKBitmap();
+
+            var info = e.Info;
+            var surface = e.Surface;
+
+            float scale = Math.Min((float)info.Width / skBitmap.Width, (float)info.Height / skBitmap.Height);
+            float x = (info.Width - scale * skBitmap.Width) / 2;
+            float y = (info.Height - scale * skBitmap.Height) / 2;
+            SKRect destRect = new SKRect(x, y, x + scale * skBitmap.Width,
+                y + scale * skBitmap.Height);
+
+            using (var paint = new SKPaint())
+            {
+                paint.IsAntialias = true;
+                paint.FilterQuality = SKFilterQuality.None;
+
+                e.Surface.Canvas.Clear();
+                e.Surface.Canvas.DrawBitmap(skBitmap, destRect, paint);
+            }
+        }
+    }
+}
